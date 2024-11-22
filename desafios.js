36841bfaf232ccd8fd69c5e0926c39affd6cf008
Amarilla-ss <manuela.alb43@icloud.com>
2024-11-21 20:45:16 -0500

    presentarmejs

---
+++ desafios.js
@@ -0,0 +1,17 @@
+let miNombre = "Manuela";
+const miEdad =  21;
+//console.log(`Hola, soy ${miNombre} y tengo ${miEdad} años`);
+
+
+
+
+function presentarme() {
+let miNombre = "Manuela";
+const miEdad = 21;
+console.log(`Hola, soy ${miNombre} y tengo ${miEdad} años`);
+} 
+
+
+presentarme();
+
+
